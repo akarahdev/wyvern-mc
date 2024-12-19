f8c037d116dd1aa31f79ed5df185b8de621af4d0
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-19 10:05:40 -0500

    feat: add networking skeleton

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -8,7 +8,6 @@ use voxidian_protocol::packet::PacketBuf;
 use crate::ConnectionHandle;
 
 pub struct Server {
-    tcp_listener: TcpListener,
     connections: Vec<ConnectionHandle>,
     events: Vec<fn(PacketBuf)>
 }
@@ -16,14 +15,10 @@ pub struct Server {
 static SERVER_EXECUTOR: LazyLock<Executor> = LazyLock::new(|| { Executor::new() });
 impl Server {
     pub fn spawn(address: String) {
-        SERVER_EXECUTOR.spawn(async move {
-            let listener = TcpListener::bind(address).await.unwrap();
-            let server = Server {
-                tcp_listener: listener,
-                connections: Vec::new(),
-                events: Vec::new()
-            };
-            server.start();
-        }).detach();
+        let server = Server {
+            connections: Vec::new(),
+            events: Vec::new()
+        };
+        server.start();
     }
 }
--- src/server/net.rs
+++ src/server/net.rs
@@ -1,13 +1,30 @@
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 use async_executor::LocalExecutor;
 use voxidian_protocol::packet::PacketBuf;
 use crate::Server;
 
 impl Server {
     pub fn start(self) {
-        loop {
-            for connection in &self.connections {
-                // do something
+        let executor = Rc::new(LocalExecutor::new().leak());
+        let in_task_executor = executor.clone();
+        executor.spawn(async move {
+            loop {
+                for connection in &self.connections {
+                    let conn = connection.clone();
+                    let sub_task_executor = in_task_executor.clone();
+                    in_task_executor.spawn(async move {
+                        // TODO: read packet byte(s)
+                    }).detach();
+
+                    let conn = connection.clone();
+                    let sub_task_executor = in_task_executor.clone();
+                    in_task_executor.spawn(async move {
+                        // TODO: write packet byte(s)
+                    }).detach();
+                }
             }
-        }
+        }).detach();
+
     }
 }
