f7a1e3ffad5fbd4bf6f3b19d814415dbee0ce345
akarahdev <endisticbutitsforgit@gmail.com>
2024-12-31 18:21:36 -0500

    feat: check in missing files

---
+++ wyvern-mc/src/scheduler/parameters.rs
@@ -0,0 +1,9 @@
+pub trait TaskParameter {
+    fn fetch() -> Self;
+}
+
+impl<A: TaskParameter, B: TaskParameter> TaskParameter for (A, B) {
+    fn fetch() -> Self {
+        (A::fetch(), B::fetch())
+    }
+}
